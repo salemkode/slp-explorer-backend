2273897d40af3d665cb7c1e83ae141801ffc28b4
Salem Mohammed Shamakh <salem.m.s2030@gmail.com>
2022-06-01 09:56:28 +0300

    Add reverse function in util file

--- src/util/slice.ts
+++ src/util/slice.ts
@@ -1,3 +1,8 @@
 export function slice<T>(array: T[], start?: number, end?: number): T[] {
   return array.slice().slice(start, end);
 }
+
+//
+export function reverse<T>(array: T[]): T[] {
+  return array.slice().reverse();
+}
